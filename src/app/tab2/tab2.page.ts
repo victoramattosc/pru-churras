9c2ad9a93297c605aae1d22b09c82c5c60dafbdf
victoramattosc <victorcarbelotti0306@gmail.com>
2023-03-16 12:31:34 -0300

    tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,73 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  valendo= 1;
+  pont1= 0;
+  part1= 0;
+  pont2= 0;
+  part2= 0;
+
   constructor() {}
+
+
+  botaoTruco1(){
+    this.valendo = 1;
+  }
+
+  botaoTruco3(){
+    this.valendo = 3;
+  }
+
+  botaoTruco6(){
+    this.valendo = 6;
+  }
+
+  botaoTruco9(){
+    this.valendo = 9;
+  }
+
+  botaoTruco12(){
+    this.valendo = 12;
+  }
+
+  botaoMais1(){
+    this.pont1 = this.pont1 + this.valendo;
+    if(this.pont1 >= 12){
+      this.part1 += 1;
+      this.pont1 = 0
+    }
+    this.valendo = 1;
+  }
+
+  botaoMenos1(){
+    this.pont1 = this.pont1 - this.valendo;
+    if(this.pont1 < 0){
+      this.pont1 = 0
+    }
+  }
+
+  botaoMais2(){
+    this.pont2 = this.pont2 + this.valendo;
+    if(this.pont2 >= 12){
+      this.part2 += 1;
+      this.pont2 = 0
+    }
+    this.valendo = 1;
+  }
+
+  botaoMenos2(){
+    this.pont2 = this.pont2 - this.valendo;
+    if(this.pont2 < 0){
+      this.pont2 = 0
+    }
+  }
+
+  limpar(){
+    this.pont1 = 0;
+    this.part1 = 0;
+    this.pont2 = 0;
+    this.part2 = 0;
+  }
+
 
 }
