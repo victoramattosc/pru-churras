f0ae933ba60a2793b40425fea015682b1431a10d
Victor Augusto de Mattos Carbelotti <victor.carbelotti@etec.sp.gov.br>
2023-04-28 01:09:38 -0300

    Início Tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,49 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  dado = 0
+  resultado = 0
+  resultadoFinal = 0
+  qtde = 1
+  adicional = 0
+
   constructor() {}
+
+  botaoJogar(jogar : number){
+    this.dado = jogar
+    this.resultado = (Math.floor(Math.random() * this.dado + 1))
+
+    if (this.adicional >= 1){
+    this.resultadoFinal = this.resultado + this.adicional
+    }
+    else{
+      this.resultadoFinal = this.resultado
+    }
+
+  }
+
+
+
+  addValor(){
+    this.adicional += 1
+  }
+
+  remValor(){
+    this.adicional -= 1
+    if (this.adicional < 0){
+      this.adicional = 0
+    }
+  }
+
+  addDado(){
+    this.qtde += 1
+  }
+
+  remDado(){
+    this.qtde -= 1
+    if (this.qtde <= 0){
+      this.qtde = 1
+    }
+  }
 
 }
